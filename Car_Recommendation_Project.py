51c2e57216981ef94a87c6eadc92ec1e42fa0346
Nilava7 <de.nilava@gmail.com>
2023-04-10 15:33:21 +0200

    ''

--- Car_Recommendation_Project.py
+++ Car_Recommendation_Project.py
@@ -192,13 +192,16 @@ with model_training:
         }
         d_top=pd.DataFrame(d_top)
 
+
+        d_top['Score(out of 5)'] = scaler.fit_transform(d_top[['Score(out of 5)']]) * 5
+        d_top['Score(out of 5)'] =  d_top['Score(out of 5)'].apply(lambda x: round(x,2))
+
         pr = pr[pr['Car Brand'].isin(d_top['Car Brand'].unique())]
         pr = pr[pr['Model'].isin(d_top['Model'].unique())]
         st.write(d_top)
         st.write(pr)
-        d_top = pd.merge(d_top.pipe(pipe_reset_index),pr.pipe(pipe_reset_index)).drop('Ratings',axis =1).rename(columns = {'Pseudo_Rating':'Ratings'})
-        d_top['Score(out of 5)'] = scaler.fit_transform(d_top[['Score(out of 5)']]) * 5
-        d_top['Score(out of 5)'] =  d_top['Score(out of 5)'].apply(lambda x: round(x,2))
+        d_top = pd.merge(d_top.pipe(pipe_reset_index), pr.pipe(pipe_reset_index)).drop('Ratings', axis=1).rename(
+            columns={'Pseudo_Rating': 'Ratings'})
         # d_top['Rank'] = [i + 1 for i in range(len(d_top))]
         # d_top.set_index('Rank', inplace=True)
         # d_top.index = [i + 1 for i in range(len(d_top))]
